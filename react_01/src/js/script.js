fcecbcd419845d027435ba001d69e237ce6a9ebd
Muhil Varnan <muhilvarnan.v@gmail.com>
2017-02-10 12:25:07 +0530

    component and methods

--- react_01/src/js/script.js
+++ react_01/src/js/script.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
+    constructor() {
+      super()
+      this.title = "Mr."
+    }
+    getLastName() {
+      return "varnan"
+    }
     render() {
+      const name = "Muhil"
       return (
-        <h1>Hello React.. It workeds</h1>
+        <h1>Hello React.. It workeds {this.title} {name} {this.getLastName()}</h1>
       );
   }
 }
