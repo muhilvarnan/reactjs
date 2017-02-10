4cbf9d2047210bf15911cd1201a69e58342612e9
muhilvarnan <muhilvarnan.v@gmail.com>
2017-02-10 13:12:08 +0530

    react state

--- react_01/src/js/components/Layout.js
+++ react_01/src/js/components/Layout.js
@@ -7,16 +7,20 @@ export default class Layout extends React.Component {
     constructor() {
       super();
       this.title = "Mr.";
+      this.state = {"name":"muhil"}
     }
     getLastName() {
       return "varnan";
     }
     render() {
+      setTimeout(() => {
+        this.setState({name:"ganesh"})
+      }, 1000);
       const name = "Muhil";
       return (
         <div>
         <Header />
-        <h1>Hello React.. It workeds {this.title} {name} {this.getLastName()}</h1>
+        <h1>Hello React.. It workeds {this.title} {this.state.name} {this.getLastName()}</h1>
         <Footer />
         </div>
       );
