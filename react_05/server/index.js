18978b2e0d8ac5c87cfe99264402f58cb907fc7f
Muhil Varnan <muhilvarnan.v@gmail.com>
2017-03-20 23:28:04 +0530

    react 3

--- react_05/server/index.js
+++ react_05/server/index.js
@@ -1,11 +1,23 @@
 import express from "express";
 import path from "path";
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from "../webpack.config.dev";
+import webpackHotMiddleware from 'webpack-hot-middleware';
+
 let app = express();
 
+const complier = webpack(webpackConfig);
+app.use(webpackMiddleware(complier, {
+  hot:true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfo:true
+}));
+app.use(webpackHotMiddleware(complier));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 })
 
-app.listen(3000, () => console.log("running on localhost:3000"))
+app.listen(3001, () => console.log("running on localhost:3000"))
---
+++ react_05/webpack.config.dev.js
@@ -0,0 +1,31 @@
+import path from "path";
+import webpack from "webpack";
+
+export default {
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
+  output: {
+    path: '/',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+
+  ],
+  module: {
+    loaders: [
+      {
+        test:/\.js$/,
+        include: path.join(__dirname, 'client'),
+        loaders:['react-hot', 'babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extentions: [' ', '.js']
+  }
+}
