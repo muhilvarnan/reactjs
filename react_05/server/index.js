fb0386372fb76c5f75600b38dd497ee41840ce31
Muhil Varnan <muhilvarnan.v@gmail.com>
2017-03-23 01:03:19 +0530

    react 15 16

--- react_05/server/index.js
+++ react_05/server/index.js
@@ -7,12 +7,14 @@ import webpackMiddleware from 'webpack-dev-middleware';
 import webpackConfig from "../webpack.config.dev";
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import users from "./routes/users";
+import auth from "./routes/auth";
 
 let app = express();
 
 app.use(bodyParser.json());
 
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 const complier = webpack(webpackConfig);
 app.use(webpackMiddleware(complier, {
   hot:true,
@@ -25,4 +27,4 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
-app.listen(1337, () => console.log("running on localhost:1337"))
+app.listen(8000, () => console.log("running on localhost:8000"))
